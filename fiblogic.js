c102b364254f17f4cea86dc49aef9d6e1fd9721d
sambw110922 <113291458+sambw110922@users.noreply.github.com>
2026-07-02 19:36:26 +0100

    Update fiblogic.js
    A complete rewrite of the fiblogic file.

--- fiblogic.js
+++ fiblogic.js
@@ -1,175 +1,134 @@
 /*
-    19th December 2025.
+    fiblogic js rewrite.
+    02 July 2026.
 */
 
-//  This function gets the user input for the sequence.
+//  This is the global array that holds the results of the calculations.
+var fibSequenceList = [];
+
+//  The maximum and minimum that the user is allowed.
+const max_num = 100;
+const min_num = 0;
+const max_times = 50;
+const min_times = 0;
+
+//  Calculate the fibonacci.
+function FibCalculate(noTimes){
+
+    for(let i = noTimes; i > 0; i--){
+
+        let lastNum = fibSequenceList[fibSequenceList.length-1];
+        let penultimateNum = fibSequenceList[fibSequenceList.length-2];
+        let result = lastNum + penultimateNum;
+
+        fibSequenceList.push(result);
+
+    }
+
+    DisplayResults();
+
+}
+
+//  This displays the results.
+function DisplayResults(){
+
+    
+    if(document.getElementById("resultsList")){
+        document.getElementById("resultsList").remove();
+    }
+    
+    let resultsList = document.createElement("ul");
+    resultsList.id = "resultsList";
+    resultsList.className = "";
+    resultsList.className = "resultsList";
+
+    for(let i = 0; i < fibSequenceList.length; i++){
+
+        var resultsListItem = document.createElement("li");
+        
+        if(i < fibSequenceList.length-1){
+            resultsListItem.innerHTML = fibSequenceList[i] + ", ";
+        } else {
+            resultsListItem.innerHTML = fibSequenceList[i] + ".";
+        }
+        
+        resultsListItem.className = "";
+        resultsListItem.className = "resultsListItem";
+        
+        resultsList.appendChild(resultsListItem);
+    }
+
+    document.getElementById("listContainer").appendChild(resultsList);
+
+}
+
+//  This validates the user's inputs.
+//  If inputType is 1, then the input to check is a number.
+//  If inputType is 2, then the input to check is the number of times.
+function ValidateUserInputs(inputType, number){
+
+    //  The return flag. Is false by default.
+    let isValidInput = false;
+
+    switch(inputType){
+        case 1:
+            if(number > min_num && number < max_num){
+                isValidInput = true;
+            }
+            break;
+        case 2:
+            if(number > min_times && number < max_times){
+                isValidInput = true;
+            }
+            break;
+    }
+
+    return isValidInput;
+
+}
+
+//  Gets the user's input.
 function GetUserInput(){
 
-    var noTime = parseInt(document.getElementById("txtTimesCalc").value);
     var num1 = parseInt(document.getElementById("txtFirstNum").value);
     var num2 = parseInt(document.getElementById("txtSecondNum").value);
+    var noTimes = parseInt(document.getElementById("txtTimesCalc").value);
 
-    if(ValidateSequenceNums(num1) == true && ValidateSequenceNums(num2) == true){
+    let checkValidNum1 = ValidateUserInputs(1, num1);
+    let checkValidNum2 = ValidateUserInputs(1, num2);
+    let checkValidNoTimes = ValidateUserInputs(2, noTimes);
 
-        if(ValidateTimesNum(noTime) == true){
+    //  Only continue if all are true.
+    if(checkValidNum1 == true && checkValidNum2 == true && checkValidNoTimes == true){
 
-            var sequence = [num1, num2];
+        //  Reset the array.
+        fibSequenceList = [];
 
-            //  Perform the fibonacci
-            Fibonacci(noTime, sequence);
+        fibSequenceList.push(num1);
+        fibSequenceList.push(num2);
 
-        } else {
-
-            //  display error message
-            document.getElementById("errorTimer").className = "";
-            document.getElementById("errorTimer").className = "errorPanel";
-
-        }
-
+        FibCalculate(noTimes);
 
     } else {
-
-        //  display error message
-        document.getElementById("errorSequence").className = "";
-        document.getElementById("errorSequence").className = "errorPanel";
-
+        window.alert("Please enter a valid number to continue.");
     }
 
 }
 
-//  This validates the number of times
-function ValidateTimesNum(timesNum){
+//  The start function.
+function fibAppStart(){
 
-    var isValid = false;
-
-    const maxTimes = 50;
-
-    if(timesNum > 0 && timesNum <= maxTimes){
-
-        isValid = true;
-
-    }
-
-    return isValid;
-
-}
-
-//  This validates the sequence numbers
-function ValidateSequenceNums(sequenceNum){
-
-    var isValid = false;
-
-    const maxSequence = 100;
-
-    if(sequenceNum > 0 && sequenceNum <= maxSequence){
-        
-        isValid = true;
-
-    }
-
-    return isValid;
-
-}
-
-//  This function actually calculates the sequence
-//  noTime = the number of times to perform the sequence
-//  sequence = the actual list
-function Fibonacci(noTime, sequence){
-
-    if(noTime > 0){
-
-        let lastNum = sequence[sequence.length-1];
-        let penultimateNum = sequence[sequence.length-2];
-        
-        let num3 = penultimateNum + lastNum;
-
-        sequence.push(num3);
-
-        --noTime;
-
-        Fibonacci(noTime, sequence);
-
-    } else {
-
-        DisplayResults(sequence);
-
-    }
-
-}
-
-//  Displays the result
-function DisplayResults(sequence){
-
-    //  Display the results
-    document.getElementById("results").className = "";
-    document.getElementById("results").className = "showResults";
-
-    //var sequenceStr = "";
-
-    for(let i = 0; i < sequence.length; i++){
-
-        setTimeout(function(){
-
-            var newPoint = document.createElement("li");
-            
-            newPoint.className = "fibNum";
-            newPoint.innerHTML = sequence[i].toString();
-
-            //  only add a comma if not at the end of sequence
-            if(i < sequence.length-1){
-    
-                //sequenceStr = sequenceStr  + ", ";
-                newPoint.innerHTML += ", ";
-
-            }
-
-            document.getElementById("resultList").append(newPoint);
-
-        //  Because we want the results to fade in after each other, 
-        //  the length of the animation duration should increase slightly
-        //  for each result
-        }, i * 1000);
-
-    }
-
-}
-
-//  The start up function 
-function init(){
-
-    //  hide the results on start
-    document.getElementById("results").className = "";
-    document.getElementById("results").className = "hideResults";
-
-    //  Hide the error messages
-    document.getElementById("errorSequence").className = "";
-    document.getElementById("errorSequence").className = "hideErrorPanel";
-
-    document.getElementById("errorTimer").className = "";
-    document.getElementById("errorTimer").className = "hideErrorPanel";
-
-    //  For the sequence error message
-    document.getElementById("btnCloseSequenceErrorMsg").addEventListener("click", function(){
-        document.getElementById("errorSequence").className = "";
-        document.getElementById("errorSequence").className = "hideErrorPanel";
-    });
-
-    //  For the timer error message
-    document.getElementById("btnCloseTimerErrorMsg").addEventListener("click", function(){
-        document.getElementById("errorTimer").className = "";
-        document.getElementById("errorTimer").className = "hideErrorPanel";
-    });
-
-    //  The click function of the submit form
     document.getElementById("btnCalculate").addEventListener("click", function(e){
 
+        //  This prevents a form's default action.
         e.preventDefault();
 
-        //  Gets the user input
+        //  Gets the user's input.
         GetUserInput();
 
     });
 
 }
 
-init();
+//  The start function.
+fibAppStart();
